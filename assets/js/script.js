2fd52138f87c288862d552c14a3780cbf67930a9
Bethany Jean <bethanyjeanbg@gmail.com>
2022-04-10 15:37:27 -0400

    updated API pulls

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,19 @@
 var ninjaKey = "o+av+xqRbo9t4LzpYZYrTg==BIM0Nl0wB5X2Kmk1";
-var consumerSecret = "3bb95ff8019f4e47b0bd89e4e30e18d7";
-var oauth_signature_method = "HMAC-SHA1";
-var oauth_timestamp = new Date();
-// var date = today.getFullYear()+'-'+(today.getMonth()+1)+'-'+today.getDate();
-var oauth_nonce = "oauth_timestamp" + Math.random();
-var oauth_version = "1.0";
+var newSmoothie = {};
 
 
-// fetch("https://cors-anywhere.herokuapp.com/" + "https://www.fruityvice.com/api/fruit/all")
-//     .then(function(response){
-//         response.json().then(function(data){
-//             console.log(data);
-//         })
-//     })
-// var fatStuff = fetch("https://platform.fatsecret.com/js?key=a897f4240bf2465d9b6050fc49d098f0")
+var swanson = function() {
+    var swansonUrl = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+
+    fetch(swansonUrl).then(function(response) {
+        console.log(response);
+        response.json().then(function(data) {
+          console.log(data);
+          $("#swanson").append(data);
+        });
+    });
+};
+
 
 var query = '3lb carrots and a chicken sandwich'
 $.ajax({
@@ -29,26 +29,4 @@ $.ajax({
     }
 });
 
-// fatsecret.setContainer("my_container");
-// fatsecret.setCanvas("home");
-
-// fetch("https://platform.fatsecret.com/js?key=a897f4240bf2465d9b6050fc49d098f0")
-//     .then(function (response) {
-//       response.json().then(function (data) {
-//         console.log(data)
-//       })
-//     });
-
-//   fetch("https://platform.fatsecret.com/rest/server.api?food_id=33691&method=food.get.v2&oauth_consumer_key=eb5c07ec22794f91974170fd9846e255&oauth_nonce=1234&oauth_signature=eb5c07ec22794f91974170fd9846e255&oauth_signature_method=HMAC-SHA1&oauth_timestamp=1245126631&oauth_version=1.0")
-//   .then(function (response) {
-//     response.json().then(function (data) {
-//       console.log(data)
-//     })
-//   });
-
-fetch("https://platform.fatsecret.com/rest/server.api" + consumerSecret + oauth_signature_method + oauth_timestamp + oauth_nonce + oauth_version)
-.then(function (response) {
-          response.json().then(function (data) {
-            console.log(data)
-          })
-        });
+swanson();
