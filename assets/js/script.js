0c2a66a57b67317275c8b61d3338f1e289d386a8
Bethany Jean <bethanyjeanbg@gmail.com>
2022-04-12 00:22:16 -0400

    dynamically added ingredients. ingredients can now be dragged and dropped into the Smoothie container

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,14 @@
 
 var ninjaKey = "o+av+xqRbo9t4LzpYZYrTg==BIM0Nl0wB5X2Kmk1";
-var newSmoothie = {};
-var fruitButton= document.getElementById("Fruit-button");
-var baseButton= document.getElementById("Base-button");
-var ptnsuppButton= document.getElementById("Protein-supplements-button");
-var closeListButton= document.getElementById("Close-list");
-var fruitList= document.getElementById("fruitList");
-var baseList= document.getElementById("baseList");
-var ptnsuppList= document.getElementById("ptnsuppList");
+// var newSmoothie = {};
+var ingredients = {
+    fruit: ["strawberry", "banana", "apple"],
+    base: ["coconut milk", "orange juice"],
+    ptnsupp: ["pea protein", "Peanut Butter"],
+    smoothie: ["poison"],
+};
+
+
 
 
 var swanson = function() {
@@ -37,11 +38,67 @@ var swanson = function() {
 //     }
 // });
 
-// swanson();
+swanson();
 
-$(".draggable").draggable();
+var createIngredient = function(ingredientText, ingredientList) {
+    var ingredientLi = $("<li>").addClass("list-item");
+    var ingredientP = $("<span>")
+        .addClass("ingredient")
+        .text(ingredientText);
+    ingredientLi.append(ingredientP);
+    $("#list-" + ingredientList).append(ingredientLi);
+};
 
-$(".dropzone").droppable();
+var loadIngredients = function() {
+    $.each(ingredients, function(list, arr){
+        arr.forEach(function(ingredient){
+            createIngredient(ingredient, list);
+        });
+    });
+};
 
+// $(".ingredient-list").draggable();
 
+$(".card .ingredient-list").sortable({
+    //enable across ingredient-lists
+    connectWith: $("#list-smoothie"),
+    scroll: false,
+    tolerance: "pointer",
+    helper: "clone",
+    activate: function(event, ui){
+        $(this).addClass("dropover");
+    },
+    deactivate: function(event, ui){
+        $(this).removeClass("dropover");
+    },
+    over: function(event) {
+        $(event.target).addClass("dropover-active");
+    },
+    out: function(event) {
+        $(event.target).removeClass("dropover-active");
+    },
+      update: function() {
+        var tempArr = [];
+
+            // loop over current set of children in sortable list
+            $(this)
+                .children()
+                .each(function() {
+                    // save values in temp array
+                    tempArr.push({
+                        text: $(this)
+                        .find("p")
+                        .text()
+                        .trim(),
+                    });
+                });
+           ingredients[smoothie] = tempArr;
+        }
+});
+
+var saveSmoothie = function() {
+    localStorage.setItem("smoothie", JSON.stringify(newSmoothie))
+};
+
+loadIngredients();
 
