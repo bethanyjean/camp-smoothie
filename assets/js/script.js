8391fc2cf809a6922322433123266df06c96aa67
Bethany Jean <bethanyjeanbg@gmail.com>
2022-04-11 21:27:49 -0400

    added responsiveness and arranged lists

--- assets/js/script.js
+++ assets/js/script.js
@@ -39,83 +39,9 @@ var swanson = function() {
 
 // swanson();
 
-// When an ingredient list is chosen, the other buttons are hidden
-// The close list button becomes visible
-// var hideButtons = function() {
-//     fruitButton.style.display = "none";
-//     baseButton.style.display = "none";
-//     ptnsuppButton.style.display = "none";
-//     closeListButton.style.display = "block";
-// };
+$(".draggable").draggable();
 
-
-// adding event listeners to all the ingredient list buttons
-// when clicked, the ingredient list is displayed
-// fruitButton.addEventListener("click", function(event){
-//     event.preventDefault()
-//     hideButtons();
-//     fruitList.style.display="block"
-// }) 
-
-// baseButton.addEventListener("click", function(event){
-//     event.preventDefault()
-//     hideButtons();
-//     baseList.style.display="block"
-// }) 
-
-// ptnsuppButton.addEventListener("click", function(event){
-//     event.preventDefault()
-//     hideButtons();
-//     ptnsuppList.style.display="block"
-// }) 
-
-// closeListButton.addEventListener("click", function(event){
-//     event.preventDefault();
-//     fruitButton.style.display = "block";
-//     baseButton.style.display = "block";
-//     ptnsuppButton.style.display = "block";
-//     closeListButton.style.display = "none";
-//     fruitList.style.display = "none";
-//     ptnsuppList.style.display = "none";
-//     baseList.style.display = "none"
-// })
-
- $(".draggable").draggable();
-
-$(".draggable").sortable( {
-    connectWith: $(".ingredient-list"),
-    scroll: false,
-    tolerance: "pointer",
-    helper: "clone",
-    activate: function(event, ui) {
-        $(this).addClass("dropover");
-    },
-    deactivate: function(event, ui) {
-        $(this).removeClass("dropover");
-    },
-    over: function(event) {
-        $(event.target).addClass("dropover-active");
-    },
-    out: function(event) {
-        $(event.target).removeClass("dropover-active");
-    },
-    update: function() {
-        var ingredient = $(this).textContent;
-        console.log("Bloody hell" + ingredient); 
-    },
-});
-
-
-$("#smoothie").droppable({
-    accept: ".draggable",
-    tolerance: "touch",
-    drop: function(event, ui) {
-        var ingredient = ui.draggable.textContent;
-        console.log(ingredient);
-    }
-});
-
-
+$(".dropzone").droppable();
 
 
 
